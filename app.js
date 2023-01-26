18b0d25ddb6ce0663bc932016f3fbc237e448b9d
Maciel <dev.macielsevilha@gmail.com>
2023-01-26 15:01:58 -0300

    criando html tubo

--- app.js
+++ app.js
@@ -1,31 +1,6 @@
-class Chao {
+class Barreira {
   constructor() {
-
-  }
-
-  novoElemento(tagName, id) {
-    const elemento = document.createElement(tagName)
-    elemento.id = id
-
-    return elemento
-  }
-
-  animar(tamanho, movimento) {
-   tamanho.style.left = movimento
+    
   }
 }
 
-let chao = new Chao()
-
-function chaoAtual() {
-
-  let criador = chao.novoElemento('div', 'chao')
-  const corpo = document.getElementById('corpo')
-
-  corpo.appendChild(criador)
-
-  chao.animar(document.getElementById('chao'), '-200px')
-
-}
-
-chaoAtual()
