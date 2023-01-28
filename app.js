0a4b702e0e91a1511419289ab5c20056997da231
Maciel <dev.macielsevilha@gmail.com>
2023-01-28 20:41:41 -0300

    fazendo a lógica tubo

--- app.js
+++ app.js
@@ -1,39 +1,61 @@
 class Barreira {
-  constructor(altura) {
-    this.altura = altura
-  }
-  criarElemento(tagName, className) {
+  novoElemento(tagName, className) {
     let elemento = document.createElement(tagName)
     elemento.className = className
 
     return elemento
-
   }
-  getAltura() {
-    let corpo = document.getElementsByClassName('corpo')[0]
-    return corpo 
-    
-  }
-  setAltura(x) {
-    this.getAltura().style.height = `${x}px`
+  sortearAbertura(corpo, altura, abertura) {
+    let alturaAtual = Math.floor(Math.random() * (altura - abertura) + abertura)
+    corpo.style.height = `${alturaAtual}px`
   }
 }
 
-let barreira = new Barreira(200)
-function barreiras(altura, abertura) {
+class Animar {
+  constructor(p) {
+    this.p = p
+  }
+  getX() {
+    return this.p.style.left.split('px')[0]
+  }
+  setX(x = 1200) {
+    this.p.style.left = `${x}px`
+  }
+}
 
-  let borda = barreira.criarElemento('div', 'borda')
-  let corpo = barreira.criarElemento('div', 'corpo')
+function barreiras() {
+ 
+  let barreira = new Barreira()
+  let borda = barreira.novoElemento('div', 'borda')
+  let corpo = barreira.novoElemento('div', 'corpo')
+  let posicao = document.getElementById('barreira')
+
+  posicao.appendChild(borda)
+  posicao.appendChild(corpo)
+
+  barreira.sortearAbertura(corpo, 200, 400)
+  barreira.sortearAbertura(corpo, 200, 400)
+  animars(posicao)
+
+}
+barreiras()
+
+function animars(p) {
+
+  let animar = new Animar(p)
+  let deslocamento = 3
+  animar.setX(1200)
+
+  setInterval(() => {
+    let get = animar.getX() - deslocamento
+    animar.setX(get)
+    if (animar.getX() < -200) {
+      animar.setX(1200)
+      
+    }
+
+  }, 5);
+
   
-  let elemento = document.getElementById('barreira')
- 
-  elemento.appendChild(borda)
-  elemento.appendChild(corpo)
 
-  let sortearAltura = Math.random() * (altura - abertura + 1) + abertura
- 
-  barreira.setAltura(sortearAltura)
- 
 }
-
-barreiras(300, 150) 
