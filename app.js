8be93f9fde75a33ea4e100798cc2dc240152d15c
Maciel <dev.macielsevilha@gmail.com>
2023-01-30 20:24:32 -0300

    criando animação personagem parte 1

--- app.js
+++ app.js
@@ -1,61 +1,99 @@
 class Barreira {
+  constructor(id = document.getElementById('barreira')) {
+    this.id = id
+  }
   novoElemento(tagName, className) {
-    let elemento = document.createElement(tagName)
+    const elemento = document.createElement(tagName)
     elemento.className = className
 
     return elemento
   }
-  sortearAbertura(corpo, altura, abertura) {
-    let alturaAtual = Math.floor(Math.random() * (altura - abertura) + abertura)
-    corpo.style.height = `${alturaAtual}px`
-  }
-}
+  sortearAbertura(corpo, abertura, altura) {
+    let numero = (Math.random() * (altura - abertura) + abertura)
+    corpo.style.height = `${numero}px`
 
-class Animar {
-  constructor(p) {
-    this.p = p
   }
   getX() {
-    return this.p.style.left.split('px')[0]
+    return this.id.style.left.split('px')[0]
   }
-  setX(x = 1200) {
-    this.p.style.left = `${x}px`
+  setX(posicao) {
+    this.id.style.left = `${posicao}px`
+  }
+}
+class Personagem {
+  constructor(pular, className) {
+    this.pular = pular
+    this.className = className
+  }
+  getX() {
+    let get = this.className.style.top.split('px')[0]
+    return get
+  }
+  setX(valor) {
+    let set = this.className.style.top = `${valor}px`
+    
   }
 }
 
 function barreiras() {
- 
   let barreira = new Barreira()
+
   let borda = barreira.novoElemento('div', 'borda')
   let corpo = barreira.novoElemento('div', 'corpo')
   let posicao = document.getElementById('barreira')
 
+  let abertura = 200
+  let altura = 300
+
   posicao.appendChild(borda)
   posicao.appendChild(corpo)
 
-  barreira.sortearAbertura(corpo, 200, 400)
-  barreira.sortearAbertura(corpo, 200, 400)
-  animars(posicao)
+  barreira.sortearAbertura(corpo, abertura, altura)
+  barreira.setX(1200)
 
-}
-barreiras()
-
-function animars(p) {
-
-  let animar = new Animar(p)
-  let deslocamento = 3
-  animar.setX(1200)
+  const DESLOCAMENTO = 3
 
   setInterval(() => {
-    let get = animar.getX() - deslocamento
-    animar.setX(get)
-    if (animar.getX() < -200) {
-      animar.setX(1200)
-      
+    barreira.setX(barreira.getX() - DESLOCAMENTO)
+
+    if (barreira.getX() < - 200) {
+      barreira.setX(1200)
+      barreira.sortearAbertura(corpo, abertura, altura)
+
     }
-
-  }, 5);
-
-  
-
+  }, 20);
 }
+
+barreiras()
+
+function personagens(alturaJogo) {
+  
+  let pular = false 
+
+  window.onkeydown = () => pular = true
+  window.onkeyup = () => pular = false
+
+  let className = document.getElementsByClassName('personagem')[0]
+  let personagem = new Personagem(pular, className)
+  let x = personagem.setX(335)
+
+  setInterval(() => {
+
+     let x = personagem.getX() - 3
+
+     if(pular == true) {
+      personagem.setX(x)
+     } else if (pular == false) {
+       if(x <= 340) {
+         personagem.setX(335)
+       }
+     }
+
+     console.log(x)
+   
+  }, 10);
+
+  console.log(personagem.getX())
+}
+
+personagens()
